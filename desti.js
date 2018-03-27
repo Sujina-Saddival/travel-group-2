201033e0b9db4bcbe727221225a2eaef14ed8ccd
varshadgowda <vdevendra@qwinix.io>
2018-03-27 16:31:42 +0530

    new commit

---
+++ desti.js
@@ -0,0 +1,51 @@
+$(document).ready(function(){
+	$(".show").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara").toggle();
+ 	 });
+	$(".show1").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara1").toggle();
+  	});
+  	$(".show2").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara2").toggle();
+  	});
+  	$(".show3").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara3").toggle();
+  	});
+  	$(".show4").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara4").toggle();
+  	});
+  	$(".show5").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara5").toggle();
+  	});
+  	$(".show6").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara6").toggle();
+  	});
+  	$(".show7").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara7").toggle();
+  	});
+  	$(".show8").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara8").toggle();
+  	});
+  	$(".show9").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara9").toggle();
+  	});
+  	$(".show10").click(function(){
+      // $(".panel-body").hide();
+      $("#Showpara10").toggle();
+  	});
+  	$(".show11").click(function(){
+      // $(".panel-body").hide();
+      debugger
+      $("#Showpara11").toggle();
+  	});
+});
