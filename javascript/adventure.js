40613904f63d4869dcf819a41d7004d0dc0411a9
NavyashreeThandavesh <nthandavesh@qwinix.io>
2018-03-27 16:41:05 +0530

    updating my files

--- javascript/adventure.js
+++ javascript/adventure.js
@@ -1,33 +1,4 @@
-$(window).scroll(function() {
-    if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
-    {
-        $('.opaque-navbar').addClass('opaque');
-    } else {
-        $('.opaque-navbar').removeClass('opaque');
-    }
-});
 
- /* affix the navbar after scroll below header */
-$('#nav').affix({
-      offset: {
-        top: $('header').height()-$('#nav').height()
-      }
-});	
-
-/* highlight the top nav as scrolling occurs */
-$('body').scrollspy({ target: '#nav' })
-
-/* smooth scrolling for scroll to top */
-$('.scroll-top').click(function(){
-  $('body,html').animate({scrollTop:0},1000);
-})
-
-/* smooth scrolling for nav sections */
-$('#nav .navbar-nav li>a').click(function(){
-  var link = $(this).attr('href');
-  var posi = $(link).offset().top+20;
-  $('body,html').animate({scrollTop:posi},700);
-})
 $(document).ready(function(){
    var $input = $("#txtAcrescimo");
     $input.val(0);
@@ -42,5 +13,6 @@ $(document).ready(function(){
     $("#image1").mouseleave(function() {
                $(".middle1").hide();
     });
+
     });
 
