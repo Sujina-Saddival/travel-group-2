8be218142d807f7c6f9536c856b6f675ea2c95e8
Harsha <hgopal@qwinix.io>
2018-03-28 12:42:42 +0530

    finished homepage

--- javascript/script.js
+++ javascript/script.js
@@ -1,84 +1,65 @@
 $(window).scroll(function() {
-  if($(this).scrollTop() > 50)  /*height in pixels when the navbar becomes non opaque*/ 
+  if($(this).scrollTop() > 50)  
   {
-      $('.opaque-navbar').addClass('opaque');
+    $('.opaque-navbar').addClass('opaque');
   } else {
-      $('.opaque-navbar').removeClass('opaque');
+    $('.opaque-navbar').removeClass('opaque');
   }
 });
 
-/* affix the navbar after scroll below header */
 $('#nav').affix({
-    offset: {
-      top: $('header').height()-$('#nav').height()
-    }
+  offset: {
+    top: $('header').height()-$('#nav').height()
+  }
 });	
 
-/* highlight the top nav as scrolling occurs */
 $('body').scrollspy({ target: '#nav' })
 
-/* smooth scrolling for scroll to top */
+
 $('.scroll-top').click(function(){
-$('body,html').animate({scrollTop:0},1000);
+  $('body,html').animate({scrollTop:0},1000);
 })
 
-/* smooth scrolling for nav sections */
 $('#nav .navbar-nav li>a').click(function(){
-var link = $(this).attr('href');
-var posi = $(link).offset().top+20;
-$('body,html').animate({scrollTop:posi},700);
+  var link = $(this).attr('href');
+  var posi = $(link).offset().top+20;
+  $('body,html').animate({scrollTop:posi},700);
 })
 
-/* google maps */
+$(document).ready(function(){
+  $('body,html').animate({
+    scrollTop: 0
+  }, 800);
+  $('.btn-go-to-top').click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 800);
+    return false;
+  });
 
-// enable the visual refresh
-// google.maps.visualRefresh = true;
 
-// var map;
-// function initialize() {
-// var mapOptions = {
-//   zoom: 15,
-//   mapTypeId: google.maps.MapTypeId.ROADMAP
-// };
-// map = new google.maps.Map(document.getElementById('map-canvas'),
-//     mapOptions);
-// 	// try HTML5 geolocation
-// if(navigator.geolocation) {
-//   navigator.geolocation.getCurrentPosition(function(position) {
-//     var pos = new google.maps.LatLng(position.coords.latitude,
-//                                      position.coords.longitude);
 
-//     var infowindow = new google.maps.InfoWindow({
-//       map: map,
-//       position: pos,
-//       content: 'Location found using HTML5.'
-//     });
+  $('body,html').animate({
+    scrollDown: 0
+  }, 800);
+  $('#btn-go-to-down').click(function () {
+    $('body,html').animate({
+      scrollDown: 0
+    }, 800);
+    return false;
+  });
 
-//     map.setCenter(pos);
-//   }, function() {
-//     handleNoGeolocation(true);
-//   });
-// } else {
-//   // browser doesn't support geolocation
-//   handleNoGeolocation(false);
-// }
-// }
 
-// function handleNoGeolocation(errorFlag) {
-// if (errorFlag) {
-//   var content = 'Error: The Geolocation service failed.';
-// } else {
-//   var content = 'Error: Your browser doesn\'t support geolocation.';
-// }
 
-// var options = {
-//   map: map,
-//   position: new google.maps.LatLng(60, 105),
-//   content: content
-// };
-
-// var infowindow = new google.maps.InfoWindow(options);
-// map.setCenter(options.position);
-// }
-// google.maps.event.addDomListener(window, 'load', initialize);
-
+  $('#hit').click(function() {
+    var text = $('.form-control').val();
+    $('#displaysubs').append('<div>' + text + '</div>');
+  });
+  $('#contact_email').on('input', function() {
+    var input=$(this);
+    var re = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    var is_email=re.test(input.val());
+    if(is_email){input.removeClass("invalid").addClass("valid");}
+    else{input.removeClass("valid").addClass("invalid");}
+  });
+});
